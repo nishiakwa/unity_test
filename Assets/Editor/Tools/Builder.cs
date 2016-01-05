935b78fa2bd8d16bf6b4e11abbb6eba30f755fe8
nishikawa <j_nishikawa@zappallas.com>
2016-01-05 17:50:55 +0900

    a

---
+++ Assets/Editor/Tools/Builder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Builder {
+
+	[UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+	public static void BuildProjectAllSceneAndroid() {
+		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+		string[] allScene = new string[EditorBuildSettings.scenes.Length];
+		int i = 0;
+		foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+			allScene[i] = scene.path;
+			i++;
+		}	
+		PlayerSettings.bundleIdentifier = "jp.co.hoge.hoge";
+		PlayerSettings.statusBarHidden = true;
+		BuildPipeline.BuildPlayer( allScene,
+		                          "hoge.apk",
+		                          BuildTarget.Android,
+		                          BuildOptions.None
+		                          );
+	}
+}
